54c36e13843e23c792ab83973e93d7756b93bb72
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-08-22 19:30:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,13 @@ class Student
   end
   
   def self.all_students_in_grade_X(x)
+    sql = <<-SQL 
+    SELECT * FROM students WHERE students.grade = ?
+    SQL
     
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def save
