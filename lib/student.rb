f1d0071f6d0bedf0c06afa81e4bdfeac3bc2b5ad
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-08-22 19:21:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,9 @@ class Student
     SELECT * FROM student WHERE name = ? LIMIT 1
     SQL
     
-    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def save
