a0be3937a3032b8ec1fefe051f178731f72aaf6b
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-08-22 19:16:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,9 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
-    
+    sql = <<-SQL
+    SELECT * FROM students
+    SQL
     
   end
 
