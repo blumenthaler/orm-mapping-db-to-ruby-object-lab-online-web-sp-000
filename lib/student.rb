ccbfb32cacd577ee961aeb6e43a2d36b58d30461
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-08-22 19:10:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,8 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    student = 
+    student = self.new
+    student.id = 
   end
 
   def self.all
